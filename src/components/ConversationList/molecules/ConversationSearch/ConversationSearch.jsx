2d8842a1d0792b68232d1a12896fe30eb4644ec2
Rida F'kih <ridafakih@gmail.com>
2021-06-10 21:01:15 -0600

    configured search input field styling

--- src/components/ConversationList/molecules/ConversationSearch/ConversationSearch.jsx
+++ src/components/ConversationList/molecules/ConversationSearch/ConversationSearch.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./ConversationSearch.module.css";
 
 function ConversationSearch() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <label className={styles.field_region}>
+        <div className={styles.input_icon}></div>
+        <input className={styles.input} type="text" placeholder="Search..." />
+      </label>
+      <button className={styles.add_conversation}></button>
+    </div>
+  );
 }
 
 export default ConversationSearch;
