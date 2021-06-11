2a387ee063d1074c2c089111cdc23bce291ee3bf
Rida F'kih <ridafakih@gmail.com>
2021-06-11 09:04:38 -0600

    added closure to generate key ids

--- src/redux/slices/chatSlice.js
+++ src/redux/slices/chatSlice.js
@@ -15,110 +15,123 @@ import { createSlice } from "@reduxjs/toolkit";
  * @typedef {{ id: number, target: User, messages: Message[], read: boolean }}
  */
 
+const initialState = {
+  conversations: [
+    {
+      id: 0,
+      read: false,
+      target: {
+        id: 0,
+        name: "Lupe Fiasco",
+        avatarUrl: "",
+      },
+      messages: [
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: false,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: true,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: false,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: true,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: false,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: true,
+        },
+        {
+          conversationId: 0,
+          timestamp: 1623391625250,
+          content: "B",
+          outgoing: true,
+        },
+      ],
+    },
+    {
+      id: 1,
+      read: true,
+      target: {
+        id: 0,
+        name: "Dua Lipa",
+        avatarUrl: "",
+      },
+      messages: [
+        {
+          conversationId: 0,
+          timestamp: new Date().getTime(),
+          content:
+            "I got two tickets to see this awesome movie, called Spongebob!",
+          outgoing: false,
+        },
+        {
+          conversationId: 0,
+          timestamp: new Date().getTime(),
+          content:
+            "I got two tickets to see this awesome movie, called Spongebob!",
+          outgoing: false,
+        },
+        {
+          conversationId: 0,
+          timestamp: new Date().getTime(),
+          content:
+            "I got two tickets to see this awesome movie, called Spongebob!",
+          outgoing: true,
+        },
+        {
+          conversationId: 0,
+          timestamp: new Date().getTime(),
+          content:
+            "I got two tickets to see this awesome movie, called Spongebob!",
+          outgoing: true,
+        },
+        {
+          conversationId: 0,
+          timestamp: new Date().getTime(),
+          content: "We don't do that here.",
+          outgoing: false,
+        },
+      ],
+    },
+  ],
+};
+
+function makeKeyGenerator(key = -1) {
+  return function () {
+    return ++key;
+  };
+}
+
+const { conversations } = initialState;
+const generateKey = makeKeyGenerator(
+  conversations[conversations.length - 1].id
+);
+
 const chatSlice = createSlice({
   name: "chat",
-  initialState: {
-    conversations: [
-      {
-        id: 0,
-        read: false,
-        target: {
-          id: 0,
-          name: "Lupe Fiasco",
-          avatarUrl: "",
-        },
-        messages: [
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: false,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: true,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: false,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: true,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: false,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: true,
-          },
-          {
-            conversationId: 0,
-            timestamp: 1623391625250,
-            content: "B",
-            outgoing: true,
-          },
-        ],
-      },
-      {
-        id: 1,
-        read: true,
-        target: {
-          id: 0,
-          name: "Dua Lipa",
-          avatarUrl: "",
-        },
-        messages: [
-          {
-            conversationId: 0,
-            timestamp: new Date().getTime(),
-            content:
-              "I got two tickets to see this awesome movie, called Spongebob!",
-            outgoing: false,
-          },
-          {
-            conversationId: 0,
-            timestamp: new Date().getTime(),
-            content:
-              "I got two tickets to see this awesome movie, called Spongebob!",
-            outgoing: false,
-          },
-          {
-            conversationId: 0,
-            timestamp: new Date().getTime(),
-            content:
-              "I got two tickets to see this awesome movie, called Spongebob!",
-            outgoing: true,
-          },
-          {
-            conversationId: 0,
-            timestamp: new Date().getTime(),
-            content:
-              "I got two tickets to see this awesome movie, called Spongebob!",
-            outgoing: true,
-          },
-          {
-            conversationId: 0,
-            timestamp: new Date().getTime(),
-            content: "We don't do that here.",
-            outgoing: false,
-          },
-        ],
-      },
-    ],
-  },
+  initialState,
   reducers: {
     conversationAdded: (state, action) => {},
     messageSent: (state, action) => {},
