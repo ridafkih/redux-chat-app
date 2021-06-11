5f6aeaceb1666473122d63269f44d8e9eb866b10
Rida F'kih <ridafakih@gmail.com>
2021-06-10 20:21:16 -0600

    added window action buttons to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "./Header.module.css";
 
 function Header() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.window_buttons}>
+        <button className={`${styles.window_button} ${styles.close}`}></button>
+        <button
+          className={`${styles.window_button} ${styles.minimize}`}
+        ></button>
+        <button
+          className={`${styles.window_button} ${styles.fullscreen}`}
+        ></button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
