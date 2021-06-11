9574a7dec487ddc82dc3a5cf454a70f2183a9c7a
Rida F'kih <ridafakih@gmail.com>
2021-06-10 20:55:10 -0600

    configured layout to be better adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Body></Body>
     </>
   );
 }
