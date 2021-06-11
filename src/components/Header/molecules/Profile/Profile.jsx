cba397cf63046d02dd5033523b7f5e668fdf9f75
Rida F'kih <ridafakih@gmail.com>
2021-06-11 11:13:46 -0600

    improved overall app overflow styling

--- src/components/Header/molecules/Profile/Profile.jsx
+++ src/components/Header/molecules/Profile/Profile.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./Profile.module.css";
 
+import faker from "faker";
+
 function Profile() {
   return (
     <div className={styles.container}>
-      <div className={styles.avatar}></div>
-      <div className={styles.name}>Kendrick Lamar</div>
+      <div className={styles.avatar_container}>
+        <img
+          src={faker.image.avatar()}
+          alt="user avatar"
+          className={styles.avatar}
+        />
+      </div>
+      <div className={styles.name}>
+        {faker.fake("{{name.firstName}} {{name.lastName}}")}
+      </div>
       <div className={styles.dropdown}></div>
     </div>
   );
