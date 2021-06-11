276b0c8fa4e8d7e9cabae3588f7632317671e05b
Rida F'kih <ridafakih@gmail.com>
2021-06-10 23:27:43 -0600

    added uislice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import chat from "./slices/chatSlice";
+import ui from "./slices/uiSlice";
 
 export const store = configureStore({
   reducer: {
     chat,
+    ui,
   },
 });
