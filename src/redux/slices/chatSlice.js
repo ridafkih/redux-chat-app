dc1c7f2887b72ad683a07c6ceab3a49f4f89988c
Rida F'kih <ridafakih@gmail.com>
2021-06-10 20:08:22 -0600

    added temp typings, and organized chat slice

--- src/redux/slices/chatSlice.js
+++ src/redux/slices/chatSlice.js
@@ -1,14 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/**
+ * A user object.
+ * @typedef {{ id: number, name: string, avatarUrl: string }} User
+ */
+
+/**
+ * A message object.
+ * @typedef {{ conversationId: number, timestamp: Date, content: string, me: boolean }} Message
+ */
+
+/**
+ * A conversation object.
+ * @typedef {{ id: number, target: User, messages: Message[] }}
+ */
+
 const chatSlice = createSlice({
   name: "chat",
-
   initialState: {
     conversations: [],
   },
-
-  reducers: {},
+  reducers: {
+    conversationAdded: (state, action) => {},
+    messageSent: (state, action) => {},
+    messageReceived: (state, action) => {},
+  },
 });
 
-export const {} = chatSlice.actions;
+export const { conversationAdded, messageSent, messageReceived } =
+  chatSlice.actions;
+
 export default chatSlice.reducer;
