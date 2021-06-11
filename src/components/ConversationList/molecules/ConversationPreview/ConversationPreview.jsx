478f4ebe38deab88ebb87b1cac40e442870a3df8
Rida F'kih <ridafakih@gmail.com>
2021-06-10 21:23:37 -0600

    completed placeholder conversation preview

--- src/components/ConversationList/molecules/ConversationPreview/ConversationPreview.jsx
+++ src/components/ConversationList/molecules/ConversationPreview/ConversationPreview.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./ConversationPreview.module.css";
 
 function ConversationPreview() {
-  return <div></div>;
+  return (
+    <div className={`${styles.container} ${styles.unread}`}>
+      <div className={styles.avatar_container}></div>
+      <div className={styles.metadata}>
+        <div className={styles.name}>Lupe Fiasco</div>
+        <div className={styles.preview}>
+          I got two tickets to go see this awesome show, it's called Spongebob
+          Squarepants.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ConversationPreview;
